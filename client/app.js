2c3e9398b01a58c15292acaf4a5d92bccc3f4718
zc3212521 <827058891@qq.com>
2018-03-07 19:27:18 +0800

    初始化工程项目架构

--- client/app.js
+++ client/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader' //热更替相关
-import App from './App.jsx'
+import App from './views/App.jsx'
 
 const root = document.getElementById('root')
 const render = (Component,renderMethod="render") =>{
@@ -17,7 +17,7 @@ render(App)
 
 if(module.hot){
     module.hot.accept('./App.jsx', ()=>{
-        const NextApp = require('./App.jsx').default
+        const NextApp = require('./views/App.jsx').default
         render(NextApp, 'hydrate')
     })
 }
